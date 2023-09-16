65c8fa28fe7b7d6561660d8b2e91f94a942e72ac
Kenjesan2023 <143146547+Kenjesan2023@users.noreply.github.com>
2023-09-16 21:38:41 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Function to be executed when the page loads
+function onPageLoad() {
+    alert("Welcome to My Resume Page!");
+}
+
+// Add an event listener to call onPageLoad when the page finishes loading
+window.addEventListener("load", onPageLoad);
